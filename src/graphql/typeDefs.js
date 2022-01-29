d378671c4e4bce8734b1f8488d994f814e4391a8
WandersonLontra <wandersonlontra@yahoo.com.br>
2022-01-29 00:30:06 -0300

    Adicionado recomendação por generos no User type

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -8,17 +8,3 @@ const typeArray = loadFilesSync(path.join(__dirname, 'modules' , '**' ,'*.gql'))
 const typeDefs = mergeTypeDefs(typeArray);
 
 module.exports = typeDefs;
-
-// const typeDefs = `
-//     type Movie {
-//         title: String
-//         year: Int
-//         imdbRating: Float
-//         genres: [Genre] @relationship(type: "IN_GENRE", direction: OUT)
-//     }
-
-//     type Genre {
-//         name: String
-//         movies: [Movie] @relationship(type: "IN_GENRE", direction: IN)
-//     }
-// `;
